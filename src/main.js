f3053c2e80952068915cc759780d96efd78f68e0
Ali Tokmakcı <alitokmakci@Ali-MacBook-Pro.modem.local>
2022-08-23 18:12:03 +0300

    Apollo client added

--- src/main.js
+++ src/main.js
@@ -1,12 +1,40 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import { createPinia } from 'pinia'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import {
+	ApolloClient,
+	createHttpLink,
+	InMemoryCache,
+} from '@apollo/client/core'
+
 import router from './router'
 import App from './App.vue'
 // import styles
 import './assets/app.css'
 
+// HTTP connection to the API
+const httpLink = createHttpLink({
+	// You should use an absolute URL here
+	uri: 'https://simplisaleshw.cotunnel.com/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+	link: httpLink,
+	cache,
+})
+
 // Create app
-const app = createApp(App)
+const app = createApp({
+	setup() {
+		provide(DefaultApolloClient, apolloClient)
+	},
+
+	render: () => h(App),
+})
 // Create store
 const store = createPinia()
 
